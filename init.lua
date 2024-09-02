0d8d315ef90dd8ebb52859659e0bc0913eb2d4a0
Chrinovic マーク <chrinovicmu@gmail.com>
2024-09-02 12:37:11 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ require("config.lazy")
 
 vim.opt.guicursor = "n-v-c:block-,i-ci-ve:block-blockwait700-blinkoff400-blinkon250,r-cr:hor20,o:hor50"
 vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
+vim.o.mouse = ""
+vim.api.nvim_set_keymap("n", "<C-v>", '"+p', { noremap = true, silent = true })
+
+
 vim.defer_fn(function()
 	vim.cmd([[hi Normal guibg = #000000]])
 end, 0)
